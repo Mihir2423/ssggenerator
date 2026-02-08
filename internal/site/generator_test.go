6738a62a7bb6563bfeb5b4e25ad965a87d4680ac
OPM <mihirraj444@gmail.com>
2026-02-08 12:35:58 +0530

    write more tests

--- internal/site/generator_test.go
+++ internal/site/generator_test.go
@@ -1,1 +1,77 @@
 package site
+
+import (
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/Mihir2423/ssggenerator/internal/fs"
+)
+
+type fakeDirEntry struct {
+	name  string
+	isDir bool
+}
+
+func (f fakeDirEntry) Name() string               { return f.name }
+func (f fakeDirEntry) IsDir() bool                { return f.isDir }
+func (f fakeDirEntry) Type() os.FileMode          { return 0 }
+func (f fakeDirEntry) Info() (os.FileInfo, error) { return nil, nil }
+
+type fakeReader struct {
+	entries []os.DirEntry
+	files   map[string][]byte
+	err     error
+}
+
+func (f fakeReader) ReadFile(path string) ([]byte, error) {
+	if f.err != nil {
+		return nil, f.err
+	}
+	return f.files[path], nil
+}
+
+func (f fakeReader) ReadDir(path string) ([]os.DirEntry, error) {
+	if f.err != nil {
+		return nil, f.err
+	}
+	return f.entries, nil
+}
+
+var _ fs.Reader = fakeReader{}
+
+func TestDiscoverPages_Success(t *testing.T) {
+	reader := fakeReader{
+		entries: []os.DirEntry{
+			fakeDirEntry{name: "index.md", isDir: false},
+			fakeDirEntry{name: "about.md", isDir: false},
+		},
+		files: map[string][]byte{
+			"/input/index.md": []byte("# Home"),
+			"/input/about.md": []byte("## About"),
+		},
+	}
+
+	gen := Generator{FS: reader}
+	pages, err := gen.DiscoverPages("/input", "/output")
+
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+	if len(pages) != 2 {
+		t.Errorf("expected 2 pages, got %d", len(pages))
+	}
+}
+
+func TestDiscoverPages_ReadDirError(t *testing.T) {
+	reader := fakeReader{
+		err: errors.New("permission denied"),
+	}
+
+	gen := Generator{FS: reader}
+	_, err := gen.DiscoverPages("/input", "/output")
+
+	if err == nil {
+		t.Error("expected error but got nil")
+	}
+}
--- internal/writer/html_writer_test.go
+++ internal/writer/html_writer_test.go
@@ -1,1 +1,86 @@
 package writer
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"testing"
+
+	"github.com/Mihir2423/ssggenerator/internal/site"
+)
+
+type fakeWriteCloser struct {
+	bytes.Buffer
+}
+
+func (f *fakeWriteCloser) Close() error {
+	return nil
+}
+
+type fakeFileCreator struct {
+	files  map[string]*fakeWriteCloser
+	mkErr  error
+	crtErr error
+}
+
+func (f *fakeFileCreator) Create(path string) (io.WriteCloser, error) {
+	if f.crtErr != nil {
+		return nil, f.crtErr
+	}
+	if f.files == nil {
+		f.files = make(map[string]*fakeWriteCloser)
+	}
+	fw := &fakeWriteCloser{}
+	f.files[path] = fw
+	return fw, nil
+}
+
+func (f *fakeFileCreator) MkdirAll(path string) error {
+	if f.mkErr != nil {
+		return f.mkErr
+	}
+	return nil
+}
+
+var _ FileCreator = &fakeFileCreator{}
+
+func TestHTMLWriter_Write_Success(t *testing.T) {
+	creator := &fakeFileCreator{}
+	writer := HTMLWriter{Creator: creator}
+
+	pages := []site.Page{
+		{
+			SourcePath: "/input/index.md",
+			OutputPath: "/output/index.html",
+			HTML:       "<h1>Home</h1>",
+		},
+	}
+
+	err := writer.Write(pages)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	if creator.files["/output/index.html"] == nil {
+		t.Error("expected file to be created")
+	}
+}
+
+func TestHTMLWriter_Write_MkdirError(t *testing.T) {
+	creator := &fakeFileCreator{
+		mkErr: errors.New("permission denied"),
+	}
+	writer := HTMLWriter{Creator: creator}
+
+	pages := []site.Page{
+		{
+			OutputPath: "/output/index.html",
+			HTML:       "<h1>Home</h1>",
+		},
+	}
+
+	err := writer.Write(pages)
+	if err == nil {
+		t.Error("expected error but got nil")
+	}
+}
